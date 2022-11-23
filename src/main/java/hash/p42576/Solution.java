979ff3ae8ed5f2db165e9254077847a6aa7083d6
chordpli <chordplaylist@gmail.com>
2022-11-23 23:05:42 +0900

    feat: p42576 완주하지 못한 선수

---
+++ src/main/java/hash/p42576/Solution.java
@@ -0,0 +1,40 @@
+package hash.p42576;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> check = new HashMap<>();
+        for (String s : participant) {
+            check.put(s, check.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : completion) {
+            check.put(s, check.get(s) - 1);
+        }
+
+        Set<String> keySet = check.keySet();
+        String answer = null;
+        for (String s : keySet) {
+            if (check.get(s) == 1) {
+                answer = s;
+            }
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+        String[] participant = {"lep", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        System.out.println(s.solution(participant, completion));
+    }
+
+
+}
