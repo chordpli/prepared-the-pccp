c2ab53d0632f32b211c876a1778b99ed5a5b3339
chordpli <chordplaylist@gmail.com>
2022-12-15 22:24:29 +0900

    feat: p42839 소수 찾기

--- src/main/java/bruteforce/p42839/Solution.java
+++ src/main/java/bruteforce/p42839/Solution.java
@@ -8,14 +8,15 @@ public class Solution {
 
 
     public static void main(String[] args) {
+        Solution s = new Solution();
+        String numbers = "01521";
+        System.out.println(s.solution(numbers));
 
     }
     /*
     1. 글자별로 숫자 저장.
-    2. 숫자를 조합한다. 0이 맨 앞자리에 나오면 pass
-    3. 해당 숫자가 소수인지 확인한다
-    4. set에 저장.
-    5. set size 리턴.
+    2. set에 저장.
+    3. 해당 숫자가 소수인지 확인하고 리턴
      */
 
     static String[] numbersArr;
@@ -30,20 +31,19 @@ public class Solution {
         dfs(0, "");
 
         // 4. 해당 숫자가 소수인지 확인한다.
-        return (int)savedNums.stream().filter(Solution::prime).count();
+        return (int) savedNums.stream().filter(Solution::prime).count();
 
     }
 
     static void dfs(int depth, String current) {
-        if(depth > numbersArr.length) return;
+        if (depth > numbersArr.length) return;
 
-        for (int i = 0; i < numbersArr.length ; i++) {
+        for (int i = 0; i < numbersArr.length; i++) {
             if (!check[i]) {
                 check[i] = true;
-                // 2. 숫자를 조합한다. 0이 맨 앞에 나올 경우 pass
-                // 3. set에 저장한다.
+                // 2. set에 저장한다.
                 savedNums.add(Integer.valueOf(current + numbersArr[i]));
-                dfs(depth+1, current+numbersArr[i]);
+                dfs(depth + 1, current + numbersArr[i]);
                 check[i] = false;
             }
 
@@ -51,9 +51,9 @@ public class Solution {
     }
 
     static boolean prime(int n) {
-        if(n<2) return false;
+        if (n < 2) return false;
         for (int i = 2; i * i <= n; i++) {
-            if(n%i == 0 ) return false;
+            if (n % i == 0) return false;
         }
         return true;
     }
